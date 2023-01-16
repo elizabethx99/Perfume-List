cf2323198abbddd6e969f2730969d0e777809f66
Elizabeth Deshko <elizabethrb1999@gmail.com>
2023-01-16 11:32:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {data} from "./data";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (){
+    const [perfume, setPerfume] = useState(data);
+
+    const removePerfume = (id) => {
+        let newPerfume = perfume.filter(perfume => perfume.id !== id);
+        setPerfume(newPerfume);
+    }
+
+    return(
+        <div>
+            <div className="container">
+            <h1>List of {perfume.length} perfumes</h1>
+            </div>
+
+            {perfume.map((element => {
+                const {id, perfume, image} = element;
+
+            
+
+                return(
+                    <div key={id}>
+                        <div className="container">
+                            <h2>{id} - {perfume}</h2>
+                        </div>
+                        <div className="container">
+                            <img src={image} width="300px" alt="perfume"/>
+                        </div>
+                        <div className="container">
+                            <button onClick ={() => removePerfume(id)}>Remove</button>
+                        </div>
+                    </div>
+                )
+            }))}
+            <div className="container">
+                <button onClick ={() => setPerfume([])}>Delete All</button>
+            </div>
+        </div>
+    )
 }
+
+
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const data = [
+
+  {
+
+    id: 1,
+
+    perfume: "Louis Vuitton - Attrape-Rêves",
+
+    image: "https://fraguru.com/mdimg/perfume/375x500.51016.jpg"
+  },
+
+  {
+
+    id: 2,
+
+    perfume: "Hermès - Un Jardin Sur Le Nil",
+
+    image: "https://fimgs.net/mdimg/perfume/375x500.18.jpg"
+
+  },
+
+  {
+
+    id: 3,
+
+    perfume: "Vilhelm  - Dear Polly",
+
+    image: "https://fimgs.net/mdimg/perfume/375x500.30928.jpg"
+
+  }, 
+
+  {
+
+    id: 4,
+
+    perfume: "Vilhelm  - Poets of Berlin",
+
+    image: "https://fimgs.net/mdimg/perfume/375x500.49306.jpg"
+  },
+
+  {
+
+    id: 5,
+
+    perfume: "Tom Ford - Lost Cherry",
+
+    image: "https://fimgs.net/mdimg/perfume/375x500.51411.jpg"
+
+  },
+
+  {
+
+    id: 6,
+
+    perfume: "Giorgio Armani - Sì Passione",
+
+    image: "https://fimgs.net/mdimg/perfume/375x500.48002.jpg"
+
+  }
+
+]
